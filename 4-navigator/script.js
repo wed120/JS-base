88a28d97ef372d5f7ae01f71afedc5bfdac7d399
wed120 <wed120@mail.ru>
2024-01-12 14:58:35 +0300

    dz 2

--- 4-navigator/script.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const adressLat = 1;
-const adressLong = 4;
-const positionLat = 5;
-const positionLong = 1;
-
-console.log(Math.sqrt(Math.pow((adressLong - positionLong), 2) + Math.pow(adressLat - positionLat, 2)));
